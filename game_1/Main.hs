8e10497cc1fafaca1a4bb64777fd990a37cacd30
JoaoSant0s <ricardoelric@gmail.com>
2018-06-18 18:32:16 -0300

    Adding limit area to transport the screen. Adding new velocity control [Must update max velocity flag]

--- game_1/Main.hs
+++ game_1/Main.hs
@@ -28,14 +28,18 @@ background = black
 -- | Data describing the state of the pong game.
 data PongGame = Game
   { ballLoc :: (Float, Float)  -- ^ Pong ball (x, y) location.
-  , ballVel :: (Float, Float)  -- ^ Pong ball (x, y) velocity.   
-  , reduceXVelocity:: Bool -- Reduce X velocity when respective direction have up button
-  , reduceYVelocity:: Bool -- Reduce Y velocity when respective direction have up button
-  , rotationLeft:: Bool
-  , rotationRight:: Bool
-  , rotation:: Float
-  , savedRotation:: Float
-  , ballDimension:: Float
+  , ballVel :: Float  -- ^ Pong ball velocity.
+  , ballVelAxis :: (Float, Float)
+  , limitBallVel :: (Float, Float)
+  , incrementalVel :: Float
+  , velocityIncrease :: Float
+  , reduceXVelocity :: Bool -- Reduce X velocity when respective direction have up button
+  , reduceYVelocity :: Bool -- Reduce Y velocity when respective direction have up button
+  , rotationLeft :: Bool
+  , rotationRight :: Bool
+  , rotation :: Float
+  , savedRotation :: Float
+  , ballDimension :: Float
   , directionX :: Float
   , directionY :: Float
   , reducingVelocity :: Float
@@ -45,9 +49,13 @@ data PongGame = Game
 initialState :: PongGame
 initialState = Game
   { ballLoc = (0, 0)
-  , ballVel = (50, 50)  
+  , ballVel = 0
+  , incrementalVel = 0
+  , ballVelAxis = (0, 0)
+  , limitBallVel = (-200, 200)
+  , velocityIncrease = 1
   , reduceXVelocity = False
-  , reduceYVelocity = False
+  , reduceYVelocity = True
   , rotationLeft = False
   , rotationRight = False
   , rotation = 0
@@ -62,40 +70,49 @@ initialState = Game
 moveBall :: Float    -- ^ The number of seconds since last update
          -> PongGame -- ^ The initial game state
          -> PongGame -- ^ A new game state with an updated ball position
-moveBall seconds game = game { directionY = dY', rotation = newRotation, savedRotation = sRotation, ballLoc = (x', y')}
+moveBall seconds game = game { rotation = newRotation, ballVelAxis = (vX', vY'), incrementalVel = incrementalVel', savedRotation = sRotation, ballLoc = (x', y')}
   where
     -- Old locations and velocities.
-    dX = directionX game
-    dY = directionY game
-    (x, y) = ballLoc game
-    (vx, vy) = ballVel game
+    (x, y) = ballLoc game    
+    (vX, vY) = ballVelAxis game   
+    incrementVel = incrementalVel game
+    oldSavedRotation = savedRotation game
+    oldRotation = rotation game
+    v = sqrt (vX' ** 2 + vY' ** 2)
+
+    incrementalVel' = if not (reduceYVelocity game)
+                      then incrementVel + (velocityIncrease game)
+                      else incrementVel            
 
     newRotation = if rotationRight game
-                  then (rotation game + 5)
+                  then (oldRotation + 5)
                   else if rotationLeft game
-                    then (rotation game - 5)
-                    else rotation game
+                    then (oldRotation - 5)
+                    else oldRotation
 
     sRotation = if not (reduceYVelocity game)
-                    then newRotation
-                    else savedRotation game
+                then newRotation
+                else oldSavedRotation
 
-    rad = (degToRad (savedRotation game) )
-
-    auxX = x + (sin rad) * vx * seconds * dY
-    auxY = y + (cos rad) * vy * seconds * dY
-
-    x' = if (auxX > (snd limitX) - (ballDimension game)) || (auxX < (fst limitX) + (ballDimension game))
-         then x
-         else auxX
-         
-    y' = if (auxY > (snd limitY) - (ballDimension game)) || (auxY < (fst limitY) + (ballDimension game))
-         then y
-         else auxY
+    rad = (degToRad oldSavedRotation)
     
-    dY' = if reduceYVelocity game
-          then (dY - seconds/(reducingVelocity game)) `max` 0
-          else dY
+    vX' = (vX + (sin rad) * incrementVel)
+    vY' = (vY + (cos rad) * incrementVel)
+
+    auxX = x + vX * seconds
+    auxY = y + vY * seconds    
+
+    x' = if auxX > (snd limitX)
+         then (fst limitX)
+         else if auxX < (fst limitX)
+          then (snd limitX)
+          else auxX
+         
+    y' = if auxY > (snd limitY)       
+         then (fst limitY)
+         else if auxY < (fst limitY)
+          then (snd limitY)
+          else auxY  
 
 -- | Convert a game state into a picture.
 render :: PongGame  -- ^ The game state to render.
@@ -104,8 +121,8 @@ render game =
   pictures [ball]
   where
     --  The pong ball.
-    ball = uncurry translate (ballLoc game) $ rotate (rotation game) $ color ballColor $ Line [(0, 20), (15, -10), (8, -5), (-8, -5), (-15, -10), (0, 20)]
-    ballColor = white
+    ball = uncurry translate (ballLoc game) $ rotate (rotation game) $ color ballColor $ Line [(0, 20), (12, -10), (5, -5), (-5, -5), (-12, -10), (0, 20)]
+    ballColor = dark green
 
 -- | Respond to key events.
 handleKeys :: Event -> PongGame -> PongGame
@@ -119,8 +136,8 @@ handleKeys (EventKey (SpecialKey KeyLeft) Up _ _) game = game { reduceXVelocity 
 handleKeys (EventKey (SpecialKey KeyRight) Down _ _) game = game { directionX = 1, reduceXVelocity = False, rotationRight = True}
 handleKeys (EventKey (SpecialKey KeyRight) Up _ _) game = game { reduceXVelocity = True, rotationRight = False }
 
-handleKeys (EventKey (SpecialKey KeyUp) Down _ _) game = game { directionY = 1, reduceYVelocity = False}
-handleKeys (EventKey (SpecialKey KeyUp) Up _ _) game = game { reduceYVelocity = True}
+handleKeys (EventKey (SpecialKey KeyUp) Down _ _) game = game { reduceYVelocity = False}
+handleKeys (EventKey (SpecialKey KeyUp) Up _ _) game = game { incrementalVel = 0, reduceYVelocity = True}
 --handleKeys (EventKey (SpecialKey KeyDown) Down _ _) game = game { directionY = -1, reduceYVelocity = False}
 
 --handleKeys (EventKey (SpecialKey KeyDown) Up _ _) game = game { reduceYVelocity = True}
